2e69b85d453a4843d4a09ed22f39aef5dd4641bf
abdelajalil <taki22abdo@gmail.com>
2021-02-06 00:49:05 +0100

    Create a work environment

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Layout\css\bs\bootstrap.css">
+    <link rel="stylesheet" href="Layout\css\bs\res.css">
+    <link rel="stylesheet" href="Layout\font\font\all.min.css">
+    <link rel="stylesheet" href="Layout\font\font\fontawesome.css">
+    <style>
+        
+    </style>
+</head>
+<body>
+    <div class="btn-danger btn btn-block"></div>
+</body>
+</html>
